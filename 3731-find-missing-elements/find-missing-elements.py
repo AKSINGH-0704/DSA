065a38290cf360b09d0dbae6c5c89cef6b69645f
AK Singh <ak.singh081105@gmail.com>
2026-08-04 19:38:45 +0000

    Synced 1 new accepted LeetCode submission(s). - 3731-find-missing-elements

---
+++ 3731-find-missing-elements/find-missing-elements.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findMissingElements(self, nums: list[int]) -> list[int]:
+        min_val = min(nums)
+        max_val = max(nums)
+        num_set = set(nums)
+        return [i for i in range(min_val, max_val + 1) if i not in num_set]
