fc788523b7a4cbe8b43f5aced12caa662039a678
AK Singh <ak.singh081105@gmail.com>
2026-09-02 16:08:07 +0000

    Synced 2 new accepted LeetCode submission(s). - 3875-construct-uniform-parity-array-i - 577-employee-bonus

---
+++ 3875-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniformArray(self, nums1: List[int]) -> bool:
+        even = sum(x % 2 == 0 for x in nums1)
+        odd = len(nums1) - even
+
+        if even == 0 or odd == 0:
+            return True
+
+        return len(nums1) > 1
